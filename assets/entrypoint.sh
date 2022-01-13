e4604f72828cefd058da581905ea47b8c91e4bfa
顾文远 <guwenyuan@xiaoniangao.com>
2022-01-13 12:19:15 +0800

    use persistent awtrix.jar

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -4,19 +4,21 @@ set -e
 
 AWTRIX_BETA=${AWTRIX_BETA:-false}
 AUTOUPDATE=${AUTOUPDATE:-true}
+File="/data/awtrix.jar"
 
+if [ "$AUTOUPDATE" = true ]; then
+  if [ "$AWTRIX_BETA" = true ]; then
+    AWTRIX_DL_URL=https://blueforcer.de/awtrix/beta/awtrix.jar
+  elif [ "$AWTRIX_BETA" = "nightly" ]; then
+    AWTRIX_DL_URL=https://blueforcer.de/awtrix/nightly/awtrix.jar
+  else
+    AWTRIX_DL_URL=https://blueforcer.de/awtrix/stable/awtrix.jar
+  fi
+  if [ ! -f "$File" ]; then
+    wget $AWTRIX_DL_URL -O $File
+    touch /data/1
+  fi
 
-if [ "$AUTOUPDATE" = true ] ; then
-    if [ "$AWTRIX_BETA" = true ] ; then
-        AWTRIX_DL_URL=https://blueforcer.de/awtrix/beta/awtrix.jar
-    elif  [ "$AWTRIX_BETA" = "nightly" ] ; then
-        AWTRIX_DL_URL=https://blueforcer.de/awtrix/nightly/awtrix.jar
-    else    
-        AWTRIX_DL_URL=https://blueforcer.de/awtrix/stable/awtrix.jar      
-    fi
-    
-
-wget $AWTRIX_DL_URL -O /data/awtrix.jar
 fi
 
-java -jar /data/awtrix.jar
+java -jar $File
